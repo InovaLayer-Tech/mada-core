6f55395f44ad1bd637673ea938470041ada63389
Rogélio <rogelio.claro@gmail.com>
2026-03-06 21:02:46 -0300

    feat: setup inicial do Spring Boot 4 e ligação ao PostgreSQL via Docker

---
+++ src/main/java/com/inovalayer/mada_core/MadaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.inovalayer.mada_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadaCoreApplication.class, args);
+	}
+
+}
